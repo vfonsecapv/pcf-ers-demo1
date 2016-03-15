ca9216d8aa2a20cd91482c0f629db26f91ca9ffc
Victor Fonseca <vfonseca@pivotal.io>
2016-03-15 16:59:19 -0300

    Mudar título da página inicial.

--- src/main/java/io/pivotal/pcf/sme/ers/PcfErsDemo1Application.java
+++ src/main/java/io/pivotal/pcf/sme/ers/PcfErsDemo1Application.java
@@ -1,9 +1,15 @@
 package io.pivotal.pcf.sme.ers;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableAutoConfiguration
 public class PcfErsDemo1Application {
 
     public static void main(String[] args) {
