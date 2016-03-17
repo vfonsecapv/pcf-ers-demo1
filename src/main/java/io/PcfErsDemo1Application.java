49cb1a556b8e6c6067fb26af258f76b3c13f6daa
Victor Fonseca <vfonseca@pivotal.io>
2016-03-17 12:46:49 -0300

    Demo

--- src/main/java/io/PcfErsDemo1Application.java
+++ src/main/java/io/PcfErsDemo1Application.java
@@ -2,12 +2,8 @@ package io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker
 public class PcfErsDemo1Application {
 
     public static void main(String[] args) {
